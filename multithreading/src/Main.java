79c38495f1e1e60160d4e10700a83782e349de1b
COREayan <63402298+COREayan@users.noreply.github.com>
2025-02-28 12:01:12 +0530

    first commit

---
+++ multithreading/src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                // Code that will run in a new thread
+                System.out.println("We are now in thread " + Thread.currentThread().getName());
+                System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+            }
+        });
+
+        thread.setName("New Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("We are in thread : " + Thread.currentThread().getName() + " before starting a new thread");
+        thread.start();
+        System.out.println("We are in thread : " + Thread.currentThread().getName() + " after starting a new thread");
+
+//        Thread.sleep(10000);
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A crititcal error happened in thread " + t.getName() + " the error is " + e.getMessage());
+            }
+        });
+    }
+}
