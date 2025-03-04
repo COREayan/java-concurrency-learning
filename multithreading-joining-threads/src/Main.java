227b6ec512cfc7c09a22e64df90258d79b8c9f7c
COREayan <63402298+COREayan@users.noreply.github.com>
2025-03-04 14:04:33 +0530

    update 14.05 04.03.2025
    update

--- multithreading-joining-threads/src/Main.java
+++ multithreading-joining-threads/src/Main.java
@@ -2,68 +2,69 @@ import java.math.BigInteger;
 import java.util.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.math.BigInteger;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // Main 1
-        Thread thread = new Thread(new BlockingTask());
-//        System.out.println("starting thread : " + thread.getName());
-        thread.start();
-//        System.out.println("thread started : " + thread.getName());
+    public static void main(String[] args) throws InterruptedException {
+        List<Long> inputNumbers = Arrays.asList(0L, 5L, 100000000L, 3435L, 35435L, 2324L, 4656L, 23L, 5556L);
 
-//        System.out.println("interrupting thread : " + thread.getName());
-        thread.interrupt();
-//        System.out.println("thread interrupted : " + thread.getName());
+        List<FactorialThread> threads = new ArrayList<>();
 
-        // Main 2
-        Thread thread2 = new Thread(new LongComputationTask(new BigInteger("200000"), new BigInteger("10000000")));
-        thread2.start();
-//        thread2.sleep(5000);
-        thread2.interrupt();
+        for (long inputNumber : inputNumbers) {
+            threads.add(new FactorialThread(inputNumber));
+        }
 
-        // Main 3
-        Thread thread3 = new Thread(new LongComputationTask(new BigInteger("2000000"), new BigInteger("10000000")));
-        thread3.setDaemon(true);
-        thread3.start();
-//        Thread.sleep(100);
-        thread.interrupt();
+        for (Thread thread : threads) {
+            thread.setDaemon(true);
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join(20000);
+        }
+
+        for (int i=0; i<inputNumbers.size(); i++) {
+            FactorialThread factorialThread = threads.get(i);
+            if (factorialThread.isFinished()) {
+                System.out.println("Factorial of " + inputNumbers.get(i) + " is " + factorialThread.getResult());
+            } else {
+                System.out.println("The calculation for " + inputNumbers.get(i) + " is still in progress");
+            }
+        }
     }
 
-    private static class BlockingTask implements Runnable {
+    public static class FactorialThread extends Thread {
+        private long inputNumber;
+        private BigInteger result = BigInteger.ZERO;
+        private boolean isFinished = false;
+
+        public FactorialThread(long inputNumber) {
+            this.inputNumber = inputNumber;
+        }
+
         @Override
         public void run() {
-            try {
-                Thread.sleep(100000);
-            } catch (InterruptedException e) {
-                System.out.println("Exiting blocking thread");
+            this.result = factorial(inputNumber);
+            this.isFinished = true;
+        }
+
+        public BigInteger factorial(long n) {
+            BigInteger tempResult = BigInteger.ONE;
+
+            for (long i = n; i>0; i--) {
+                tempResult = tempResult.multiply(new BigInteger(Long.toString(i)));
             }
-        }
-    }
-
-    private static class LongComputationTask implements Runnable {
-        private BigInteger base;
-        private BigInteger power;
-
-        public LongComputationTask(BigInteger base, BigInteger power) {
-            this.base = base;
-            this.power = power;
+            return tempResult;
         }
 
-        @Override 
-        public void run() {
-            System.out.println(base + "^" + power + " = " + pow(base, power));
-        }
-
-        private  BigInteger pow(BigInteger base, BigInteger power) {
-            BigInteger result = BigInteger.ONE;
-
-            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
-                if (Thread.currentThread().isInterrupted()) {
-                    System.out.println("Prematurely interrupted computation");
-                    return BigInteger.ZERO;
-                }
-                result = result.multiply(base);
-            }
+        public BigInteger getResult() {
             return result;
+        }
+
+        public boolean isFinished() {
+            return isFinished;
         }
     }
 }
